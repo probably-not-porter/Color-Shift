91ce503e57169a88883495f4e1d829335763f7ca
Porter <pelibby16@earlham.edu>
2019-05-10 18:12:31 -0400

    add particles and display changes

--- Assets/Scripts/displayBar.cs
+++ Assets/Scripts/displayBar.cs
@@ -15,7 +15,7 @@ public class displayBar : MonoBehaviour
 
     void Update() // update time bar every second
     {
-        float barWidth = controller.GetComponent<control_3>().targetTime / controller.GetComponent<control_3>().roundTime * Screen.width * 0.7f;
+        float barWidth = controller.GetComponent<control_3>().targetTime / controller.GetComponent<control_3>().roundTime * Screen.width * 0.75f;
         var selfTransform = transform as RectTransform;
         selfTransform.sizeDelta = new Vector2 (barWidth, selfTransform.sizeDelta.y);
     }
