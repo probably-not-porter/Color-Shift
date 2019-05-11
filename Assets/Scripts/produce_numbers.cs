81af7968b63897097aa824590703d964d261bf98
Porter <pelibby16@earlham.edu>
2019-05-11 16:22:01 -0400

    update comments + details

---
+++ Assets/Scripts/produce_numbers.cs
@@ -0,0 +1,20 @@
+﻿// -----
+//
+// Porter Libby
+// Color Shift 2019
+//
+// -----
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class produce_numbers : MonoBehaviour
+{
+    public GameObject plusOne; // prefab for +1 score
+
+    public void create1() // create new +1 object
+    {
+        Instantiate(plusOne, transform, false);
+    }
+}
