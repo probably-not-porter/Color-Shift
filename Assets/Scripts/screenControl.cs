8403a0e7b4a9dbcf8e84297fb2630505dfdb2e09
probably-not-porter <pelibby16@earlham.edu>
2020-08-27 18:53:32 -0400

    add support for 4x4 to screenctrl

--- Assets/Scripts/screenControl.cs
+++ Assets/Scripts/screenControl.cs
@@ -13,11 +13,14 @@ using UnityEngine.UI;
 public class screenControl : MonoBehaviour
 {
     public GameObject menuPanel;
-    public GameObject threePanel;
     public GameObject pausePanel;
     public GameObject losePanel;
 
+    public GameObject threePanel;
+    public GameObject fourPanel;
+
     public GameObject threeController;
+    public GameObject fourController;
 
     public int activescreen = 0;
 
@@ -34,10 +37,15 @@ public class screenControl : MonoBehaviour
     }
     public void play3() // start from menu and shift to 3x3
     {
-        activescreen = 1;
+        activescreen = 3;
         threeController.GetComponent<control_3>().running = true;
     }
-    public void menu() // start from menu and shift to 3x3
+    public void play4() // start from menu and shift to 3x3
+    {
+        activescreen = 4;
+        fourController.GetComponent<control_4>().running = true;
+    }
+    public void menu()
     {
         activescreen = 0;
         threeController.GetComponent<control_3>().running = false;
@@ -45,12 +53,12 @@ public class screenControl : MonoBehaviour
     }
     public void pause()
     {
-        activescreen = 2;
+        activescreen = 1;
         threeController.GetComponent<control_3>().running = false;
     }
     public void lost()
     {
-        activescreen = 3;
+        activescreen = 2;
         threeController.GetComponent<control_3>().running = false;
         threeController.GetComponent<control_3>().winstreak = 0;
     }
@@ -65,28 +73,39 @@ public class screenControl : MonoBehaviour
             pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
             losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
         }
-        else if (activescreen == 1) // 3x3 game
-        {
-            menuPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen,0f); 
-            threePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(onScreen,0f);
-            pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
-            losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
-        }
-        else if (activescreen == 2) // paused
+        else if (activescreen == 1) // paused
         {
             menuPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen,0f); 
             threePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen,0f);
             pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(onScreen, 0f);
             losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
         }
-        else if (activescreen == 3) // lost
+        else if (activescreen == 2) // lost
         {
             menuPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen,0f); 
             threePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen,0f);
             pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
             losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(onScreen, 0f);
         }
+        else if (activescreen == 3) // 3x3 game
+        {
+            menuPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+            threePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(onScreen, 0f);
+            pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+            losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+        }
+        else if (activescreen == 4) // 4x4 game
+        {
+            menuPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+            threePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(onScreen, 0f);
+            pausePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+            losePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(offScreen, 0f);
+        }
+        else
+        {
+            Debug.Log("This game state is not implemented, or is not hooked up to the screenControl Object.");
+        }
         // UPDATE POSITIONS OF SCREENS IF NECISSARY
-        
+
     }
 }
